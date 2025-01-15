b97449c9b622fc18224c6a14fcf0a28e637a877e
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-15 13:48:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@ provider "aws" {
   region = "us-east-1" # Set to your desired AWS region
 }
 
-# Create an S3 Bucket
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "terraformgithubbucket"
-  acl    = "private"
-}
-
 # Create an EC2 Instance
 resource "aws_instance" "my_instance" {
   ami           = "ami-0e2c8caa4b6378d8c"  # Update with a valid AMI ID in your region
