e7469322fcb3dc7d2cb4997b84183021382c3217
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-16 11:28:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "my_instance" {
   instance_type = "t2.micro"  # Adjust the instance type as per your needs
 
   tags = {
-    Name = "MyInstance005"
+    Name = "MyInstance06"
   }
 }
 terraform {
